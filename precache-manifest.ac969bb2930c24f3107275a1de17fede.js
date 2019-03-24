345f0ffb150b6bf38f8694c0e96792ea58ba04b5
Dima Baranov <vajnoe-dima@mail.ru>
2019-03-24 16:26:06 +0300

    Updates

---
+++ precache-manifest.ac969bb2930c24f3107275a1de17fede.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f95a1722d2919bb31e8",
+    "url": "/react-lessons/static/js/runtime~main.2f95a172.js"
+  },
+  {
+    "revision": "24f02873d8f743b393fa",
+    "url": "/react-lessons/static/js/main.24f02873.chunk.js"
+  },
+  {
+    "revision": "d9b4afe25c9b8c254272",
+    "url": "/react-lessons/static/js/1.d9b4afe2.chunk.js"
+  },
+  {
+    "revision": "24f02873d8f743b393fa",
+    "url": "/react-lessons/static/css/main.0d52a0f2.chunk.css"
+  },
+  {
+    "revision": "ad0488d8f9cd8e68cd95eec1ce11dcc4",
+    "url": "/react-lessons/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-lessons/precache-manifest.c95372bc3d9a5170477c4e421a48efd9.js"
+  "/react-lessons/precache-manifest.ac969bb2930c24f3107275a1de17fede.js"
 );
 
 workbox.clientsClaim();
